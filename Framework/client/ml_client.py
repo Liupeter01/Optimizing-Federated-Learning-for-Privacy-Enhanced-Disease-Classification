56fe817a1eb101b000be9f46edbe7be4aaebcf03
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-03-20 13:04:31 +1100

    1.[MODIFY]:all data input could be done by generate requests

--- Framework/client/ml_client.py
+++ Framework/client/ml_client.py
@@ -1,18 +1,23 @@
 import grpc
+import random
 import ml_vector_pb2
 import ml_vector_pb2_grpc
+
+def generate_requests():
+    vec = [random.uniform(0, 100) for _ in range(4)]
+
+    print(f"Sending vector: {vec}")
+    yield ml_vector_pb2.VectorRequest(vector=vec)  # 发送数据
 
 def run():
     channel = grpc.insecure_channel("localhost:50051")
     stub = ml_vector_pb2_grpc.MLServiceStub(channel)
 
-    # Example ML vector
-    ml_vector = [1.2, 2.4, 3.6, 4.8, 6.0]
+    ### Vector data are sent from generate_requests()
+    response_iterator = stub.FederatedAveraging(generate_requests())
 
-    request = ml_vector_pb2.VectorRequest(vector=ml_vector)
-    response = stub.ProcessVector(request)
-    
-    print("Server response:", response.message)
+    for response in response_iterator:
+        print(f"Received merged vector from server: {response.merged_vector}")
 
 if __name__ == "__main__":
     run()
