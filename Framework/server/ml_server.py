72c95ea220d85e67c3d455bb69fd0aa65b9fa24c
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-03-20 13:05:20 +1100

    1.[ADD]:avg could be calculate by function FederatedAveraging, FL algo could be done here!

--- Framework/server/ml_server.py
+++ Framework/server/ml_server.py
@@ -3,23 +3,59 @@ import ml_vector_pb2
 import ml_vector_pb2_grpc
 from concurrent import futures
 import time
+import threading
+from interceptor import ClientCountInterceptor 
+
+# Thread Safe global variable for recording client connections
+client_count = 0
+client_count_lock = threading.Lock()
 
 class MLServiceServicer(ml_vector_pb2_grpc.MLServiceServicer):
-    def ProcessVector(self, request, context):
-        print(f"Received vector with {len(request.vector)} elements.")
+    def __init__(self):
+        self.client_vectors = []  
+        self.lock = threading.Lock()  
+        self.client_streams = []  
+    
+    def FederatedAveraging(self, request_iterator, context):
+        # All Connected Client
+        with self.lock:
+            self.client_streams.append(context)
 
-        if len(request.vector) == 0:
-            avg = 0.0
-        else:
-            avg = sum(request.vector) / len(request.vector)
+        print("New client connected. Waiting for vectors...")
 
-        response = ml_vector_pb2.VectorResponse(
-            message=f"Processed vector, average value: {avg:.2f}"
-        )
-        return response
+        for request in request_iterator:
+            with self.lock:
+                self.client_vectors.append(request.vector)  # Store Their FL Vector
+
+            print(f"Received vector from client: {request.vector}")
+
+            with self.lock:
+                # Calculate Average Vector
+                if len(self.client_vectors) > 0:
+                    num_clients = len(self.client_vectors)
+                    vector_size = len(self.client_vectors[0])
+                    avg_vector = [
+                        sum(v[i] for v in self.client_vectors) / num_clients
+                        for i in range(vector_size)
+                    ]
+
+                    print(f"Updated averaged vector: {avg_vector}")
+
+                    # Boradcast to all the clients
+                    yield ml_vector_pb2.VectorResponse(merged_vector=avg_vector)
+
+        # Terminate Stream
+        with self.lock:
+            self.client_streams.remove(context)
+
+        return
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        interceptors=[ClientCountInterceptor()]
+    )
+
     ml_vector_pb2_grpc.add_MLServiceServicer_to_server(MLServiceServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
