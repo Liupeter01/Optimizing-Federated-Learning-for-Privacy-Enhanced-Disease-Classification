b9b4f1a83f164faf651fde5553a38678c824ee7f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-04-15 09:09:53 +1000

    1.[MODIFY]:modify init model code with dp argument

--- client/resnet18/ml_resnet18.py
+++ client/resnet18/ml_resnet18.py
@@ -1,14 +1,8 @@
-from ml_split import split
-from .ml_preprocess import preprocess_image
-from .ml_modeltraining import run_all
+#from ml_split import split
+#from .ml_preprocess import preprocess_image
+from .ml_modeltraining import run_all, run_all_with_dp
 
 # This part only needs to be run once
-
-
-def initialize(image_dir, csv_path, output_dir,  df_train_path):
-    split(csv_path)
-    preprocess_image(image_dir, csv_path, output_dir, df_train_path)
-
-
-def resnet18(image_dir, csv_path, output_dir,  df_train_path, model_path):
-    run_all(image_dir, csv_path, output_dir, df_train_path, model_path)
+def resnet18(csv_path, output_dir,  df_train_path, model_path, config = None):
+    #run_all(image_dir, csv_path, output_dir, df_train_path, model_path)
+    return run_all_with_dp(csv_path, output_dir, df_train_path, model_path, config)
